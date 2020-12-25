8876d198e32a5a8364c05301485b2429dfc41d4f
Evelarami <reloxes@hotmail.com>
2020-12-25 16:08:00 +0200

    123

---
+++ locators.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
+
+
+class LoginPageLocators:
+    LOGIN_FIELD = BasePage.find_element(By.CSS_SELECTOR, "#icon-email")
+    PASSWORD_FIELD = BasePage.find_element("#password")
+    LOGIN_BUTTON = BasePage.find_element(By.CSS_SELECTOR, ".btn.waves-effect")
+    RESET_PASSWORD_BUTTON = BasePage.find_element(By.CSS_SELECTOR, ".btn-flat")
