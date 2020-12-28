36b0d4b849f3b8e43c69e3b97b12ba032b6640b1
Evelarami <reloxes@hotmail.com>
2020-12-28 12:58:35 +0200

    base page update

--- locators.py
+++ locators.py
@@ -4,6 +4,6 @@ from pages.base_page import BasePage
 
 class LoginPageLocators:
     LOGIN_FIELD = BasePage.find_element(By.CSS_SELECTOR, "#icon-email")
-    PASSWORD_FIELD = BasePage.find_element("#password")
+    PASSWORD_FIELD = BasePage.find_element(By.CSS_SELECTOR, "#password")
     LOGIN_BUTTON = BasePage.find_element(By.CSS_SELECTOR, ".btn.waves-effect")
     RESET_PASSWORD_BUTTON = BasePage.find_element(By.CSS_SELECTOR, ".btn-flat")
--- pages/base_page.py
+++ pages/base_page.py
@@ -16,3 +16,17 @@ class BasePage:
     def find_elements(self, locator, time=10):
         return WebDriverWait(self.browser, time).until(EC.presence_of_all_elements_located(locator),
                                                        message=f"Cant find list of elements by locator {locator}")
+
+    def click(self, by_locator):
+        WebDriverWait(self.browser, 10).until(EC.visibility_of_element_located(by_locator)).click()
+
+    def assert_element_text(self, by_locator, element_text):
+        web_element = WebDriverWait(self.browser, 10).until(EC.visibility_of_element_located(by_locator))
+        assert web_element.text == element_text
+
+    def enter_text(self, by_locator, text):
+        return WebDriverWait(by_locator, 10).until(EC.visibility_of_element_located(by_locator)).send_keys(text)
+
+    def is_enabled(self, by_locator):
+        return WebDriverWait(by_locator, 10).until(EC.visibility_of_element_located(by_locator))
+
