08f32a0a43493d6936a23876a0da6f487524e144
Evelarami <39416190+Evelarami@users.noreply.github.com>
2022-09-06 11:52:41 +0300

    Delete test_login_page.py

--- tests/test_login_page.py
+++
@@ -1,8 +0,0 @@
-from pages.login_page import LoginPage
-import time
-
-def test_user_can_login(browser):
-    page = LoginPage(browser)
-    page.send_login_text("reloxes@hotmail.com")
-    page.send_password_keys("qwerty")
-    time.sleep(10)
